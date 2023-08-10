099283d94c34545a6d5af27eb58f53540c8b0930
jefriyanto-cloud <83343097+jefriyanto-cloud@users.noreply.github.com>
2023-08-10 18:39:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,6 @@ global.APIs = {
    alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
 }
 
-//buy apikey premium 0887435047326
-// Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
 global.APIKeys = {
    'https://api.zeeoneofc.my.id': 'Your Key', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
@@ -17,11 +15,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.BOT_TOKEN = "6064414009:AAF8uIOdsziVaJQL6z_jOg3XltyiMDyzQh0" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "CYASTORE_PBOB_bot" //your bot name
+global.OWNER_NAME = "CYASTORE" //your name
+global.OWNER_NUMBER = "62895638010106" //your telegram number
+global.OWNER = ["https://t.me/CYASTORE_PBOB_bot", "https://t.me/CYASTORE_PBOB_bot"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
